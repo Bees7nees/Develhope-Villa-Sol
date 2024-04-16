67c53219c6a8c34feee4766647a9d777d96f2f47
MfrMesa <berriesccs@gmail.com>
2024-04-16 21:59:07 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import Navbar from "/src/Components/Navbar"
-import CarouselMenu from "/src/Components/CarouselMenu"
-import Footer from "/src/Components/Footer"
-import HorariosRestaurante from "/src/Components/HorarioRestaurante"
-import Mensaje from "/src/Components/Mensaje"
-import MenuMobile from "/src/Components/MenuMobile"
-import TipoMenu from "/src/Components/TipoMenu"
+
 
 export default function App() {
 
   return (
     <>
-    <Navbar/>
-    <CarouselMenu/>
-    <Footer/>
-    <HorariosRestaurante/>
-    <Mensaje/>
-    <MenuMobile/>
-    <TipoMenu/>
+
     </>
   )
 }
