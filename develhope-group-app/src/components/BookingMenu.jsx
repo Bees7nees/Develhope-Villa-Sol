e2d7b9676c79002b82ed984308d20198303be3d5
Marina Matas <lmarina998@gmail.com>
2024-04-17 15:17:47 +0200

    Upload Assets

--- develhope-group-app/src/components/BookingMenu.jsx
+++ develhope-group-app/src/components/BookingMenu.jsx
@@ -1,51 +1,54 @@
-import styles from '../styles/BookingMenu.module.scss';
-import React, { useState } from 'react';
+import styles from '../Styles/BookingMenu.module.scss';
 import CalendarComponent from './CalendarBooking';
-
+import {useState, useEffect} from 'react';
 export default function BookingMenu(){
-  
-    const [showCalendarLeft, setShowCalendarLeft] = useState(false);
+    const [firstDate, setFirstDate] = useState(new Date());
+    const [lastDate, setLastDate] = useState(new Date());
+    const [nights, setNights] = useState(0);
+    function updateNights() {
+        const diferenciaEnMS = Math.abs(lastDate - firstDate);
+            const unDiaEnMS = 1000 * 60 * 60 * 24;
+            const diferenciaEnDias = Math.floor(diferenciaEnMS / unDiaEnMS);
+            setNights(diferenciaEnDias + 1);
+    }
+    useEffect(() => {
+        updateNights();
+    }, [firstDate, lastDate]);
 
-  
-    
-
-    const toggleCalendarLeft = () => {
-      setShowCalendarLeft(!showCalendarLeft);
-    };
     return (
         <div className={styles.bookingMenuDiv}>
             <div className={styles.bookingArrive}>
                 <div className={styles.iconCalendarDiv}>
-                <img src="../../assets/Icon_ Calendar.png" className={styles.iconCalendar}/>
+                <img src="../Assets/Img/Icon_ Calendar.png" className={styles.iconCalendar}/>
                 </div>
                 <div className={styles.textDivBookingMenu}>
                 <p className={styles.subtitle}>LLEGADA</p>
-                <CalendarComponent />
+                <CalendarComponent setData={setFirstDate} />
                 </div>
             </div>
             <div className={styles.arrowBooking}>
             <div className={styles.iconCalendarDiv}>
-                <img src="../../assets/Icon_ Arrow.png" className={styles.iconArrow}/>
+                <img src="../Assets/Img/Icon_ Arrow.png" className={styles.iconArrow}/>
                 </div>
              </div>
             <div className={styles.bookingLeft}>
             <div className={styles.iconCalendarDiv}>
-            <img src="../../assets/Icon_ Calendar.png" className={styles.iconCalendar}/>
+            <img src="../Assets/Img/Icon_ Calendar.png" className={styles.iconCalendar}/>
             </div>
            <div className={styles.textDivBookingMenu}>
                 <p className={styles.subtitle}>SALIDA</p>
-                <CalendarComponent />
+                <CalendarComponent setDate={setLastDate} />
                 </div>
             </div>
             <div className={styles.divideLine}></div>
             <div className={styles.bookingNight}>
             <div className={styles.iconSleepDiv}>
-                <img src="../../assets/Icon_ Sleep.png" className={styles.iconSleep}/>
+                <img src="../Assets/Img/Icon_ Sleep.png" className={styles.iconSleep}/>
             </div>
             <div>
             <div className={styles.textDivBookingMenu} >
                 <p className={styles.subtitle}>NOCHES</p>
-                <p className={styles.normalTextBooking}>1 NOCHE</p>
+                <p className={styles.normalTextBooking}>{nights} NOCHE</p>
                 
                 </div>
                
@@ -54,7 +57,7 @@ export default function BookingMenu(){
             <div className={styles.divideLine}></div>
             <div className={styles.hosts}>
             <div className={styles.iconUserpDiv}>
-                <img src="../../assets/User.png" className={styles.iconSleep}/>
+                <img src="../Assets/Img/User.png" className={styles.iconSleep}/>
             </div>
             <div className={styles.textDivBookingMenu}>
                 <p className={styles.subtitle}>HUÉSPEDES</p>
@@ -65,7 +68,7 @@ export default function BookingMenu(){
             
             <div className={styles.coupon}>
             <div className={styles.iconDiscountpDiv}>
-                <img src="../../assets/Discount.png" className={styles.iconDiscount}/>
+                <img src="../Assets/Img/Discount.png" className={styles.iconDiscount}/>
             </div>
             <div className={styles.textDivBookingMenu}>
                 <p className={styles.subtitle}>CUPÓN</p>
@@ -73,7 +76,7 @@ export default function BookingMenu(){
                 </div>
             </div>
             <div className={styles.checkButton}>
-            <img src="../../assets/Icon_ Bell.png" className={styles.iconBell}/>
+            <img src="../Assets/Img/Icon_ Bell.png" className={styles.iconBell}/>
             </div>
         </div>
     )
