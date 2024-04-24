b32295918bd5e171ed3d3ed8f654e0178d139229
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-04-24 09:49:34 +0100

    v.0.13 (Stable) : avance carusel reseña

--- develhope-group-app/src/components/ReviewCardCarousel.jsx
+++ develhope-group-app/src/components/ReviewCardCarousel.jsx
@@ -1,59 +1,131 @@
-import { useEffect, useRef, useState } from "react";
-import classes from "../Styles/ReviewCardCarousel.module.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { ReviewCard } from "./ReviewCard";
-import { IoIosArrowForward } from "react-icons/io";
-import { IoIosArrowBack } from "react-icons/io";
+import classes from "../Styles/ReviewCardCarousel.module.scss";
+import PropTypes from "prop-types";
 
-export function ReviewCardCarousel() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const slideCount = 12; // Total number of slides
-  const slideWidth = 100; // Width of each slide in %
-  const contentRef = useRef(null);
+PropTypes.shape({
+  className: PropTypes.string,
+  style: PropTypes.object,
+  onClick: PropTypes.func,
+});
 
-  // Calculate the width of each slide and set the width of the .content element
-  useEffect(() => {
-    if (contentRef.current) {
-      const contentWidth =
-        slideCount * slideWidth * (contentRef.current.offsetWidth / 100);
-      contentRef.current.style.width = `${contentWidth}px`;
-    }
-  }, []);
-
-  // Update the scroll position of the .content element when the current slide index changes
-  useEffect(() => {
-    if (contentRef.current) {
-      contentRef.current.scrollLeft =
-        currentSlide * slideWidth * (contentRef.current.offsetWidth / 100);
-    }
-  }, [currentSlide]);
-
-  // Function to handle previous button click
-  const handlePrevClick = () => {
-    setCurrentSlide((prevSlide) => (prevSlide - 1 + slideCount) % slideCount);
-  };
-
-  // Function to handle next button click
-  const handleNextClick = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % slideCount);
-  };
-
+function ArrowRight(props) {
+  const { className, style, onClick } = props;
   return (
-    <div className={classes.container}>
-      <div className={classes.wrapper}>
-        <button type="button" className={classes.prevButton} onClick={handlePrevClick}>
-          <IoIosArrowBack style={{ fill: "black" }} />
-        </button>
-        <div className={classes.contentwrapper}>
-          <div className={classes.content} ref={contentRef}>
-            {[...Array(slideCount)].map((_, index) => (
-              <ReviewCard key={index} style={{ width: `${slideWidth}%` }} />
-            ))}
-          </div>
+    <div className={className} style={{ ...style }} onClick={onClick}>
+      <svg
+        className={classes.svgRight}
+        xmlns="http://www.w3.org/2000/svg"
+        width="21"
+        height="30"
+        viewBox="0 0 21 47"
+        fill="none"
+      >
+        <path
+          d="M0.0735302 40.8833L12.9835 23.444L0.278748 5.72679L4.25323 0.357928L20.8973 23.518L3.98467 46.3643L0.0735302 40.8833Z"
+          fill="#213242"
+        />
+      </svg>
+    </div>
+  );
+}
+
+function ArrowLeft(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div className={className} style={{ ...style }} onClick={onClick}>
+      <svg
+        className={classes.svgLeft}
+        xmlns="http://www.w3.org/2000/svg"
+        width="21"
+        height="30"
+        viewBox="0 0 21 47"
+        fill="none"
+        style={{ transform: "rotate(180deg)" }}
+      >
+        <path
+          d="M0.0735302 40.8833L12.9835 23.444L0.278748 5.72679L4.25323 0.357928L20.8973 23.518L3.98467 46.3643L0.0735302 40.8833Z"
+          fill="#213242"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default function ReviewCardCarousel() {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    nextArrow: <ArrowRight />,
+    prevArrow: <ArrowLeft />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div>
+      <Slider {...settings}>
+        <div>
+          <ReviewCard />
         </div>
-        <button type="button" className={classes.nextButton} onClick={handleNextClick}>
-          <IoIosArrowForward style={{ fill: "black" }} />
-        </button>
-      </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+        <div>
+          <ReviewCard />
+        </div>
+      </Slider>
     </div>
   );
 }
--- develhope-group-app/src/main.jsx
+++ develhope-group-app/src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Router>,
-)
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
+);
