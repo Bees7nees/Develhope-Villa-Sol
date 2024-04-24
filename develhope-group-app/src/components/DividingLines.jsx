4a7c2af2410fdeaed590b6da3b698e51c62e6526
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-04-24 12:33:01 +0100

    Last minute review

--- develhope-group-app/src/components/DividingLines.jsx
+++ develhope-group-app/src/components/DividingLines.jsx
@@ -6,8 +6,14 @@ DividingLines.propTypes = {
 };
 
 export default function DividingLines({ color, length }) {
-    return (
-      <div style={{ width: "100%", height: "0px", borderTop: `3px ${color} solid`, marginTop: length }}></div>
-    );
-  }
-  
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "0px",
+        borderTop: `3px ${color} solid`,
+        marginTop: length,
+      }}
+    ></div>
+  );
+}
