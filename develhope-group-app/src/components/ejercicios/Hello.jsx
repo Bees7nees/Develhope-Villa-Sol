a73ac78671c79ebf96aca4cd7830014e2caabce8
EMILIOFC <julitocazalla96@gmail.com>
2024-04-26 12:41:13 +0200

    eliminado carpetas

--- develhope-group-app/src/components/ejercicios/Hello.jsx
+++
@@ -1,8 +0,0 @@
-import Message from "./Message"
-import Style from "../Hello.module.css"
-
-export default function Hello() {
-  return (
-    <Message/>
-  )
-}
--- develhope-group-app/src/components/ejercicios/Message.jsx
+++
@@ -1,8 +0,0 @@
-
-import Style from "../Hello.module.css"
-
-export default function Message() {
-  return (
-    <p className={Style.textGrande}> What a beautiful day! </p>
-  )
-}
