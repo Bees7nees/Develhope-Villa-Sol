eb88d141026809390dc4aaecc17e2e0369aee5e0
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-04-29 23:12:03 +0100

    Update vite.config.js

--- develhope-group-app/vite.config.js
+++ develhope-group-app/vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
     port: 5178,
   }
-=======
->>>>>>> TermsConditions-&-SPA
 })
