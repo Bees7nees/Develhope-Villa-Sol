f9b9bc6b3888587554e8c8710e258a28404a672e
JaviViCa <vicaxa2010@gmail.com>
2024-04-30 13:58:32 +0200

    CTA Reservas

--- develhope-group-app/src/Components/PopupReservas.jsx
+++ develhope-group-app/src/Components/PopupReservas.jsx
@@ -1,66 +1,116 @@
 import styles from "../Styles/PopupReservas.module.css";
+import { useState } from "react";
 
 export default function PopupReservas() {
-    const fecha1 = new Date();  {/* recoge las fechas */}
+    const fecha1 = new Date(); /* recoge las fechas */
     const fecha2 = new Date(fecha1);
     fecha2.setDate(fecha1.getDate() + 1);
-
-    const adultos = 2;   {/* recoge los ocupantes */}
+    const noches = (fecha2 - fecha1) / 86400000; /*Calculo de noches*/
+  
+    const adultos = 2; /* recoge los ocupantes */
     const ninyos = 0;
-    const ocupantes = adultos + ninyos;  
+    const ocupantes = adultos + ninyos;
+  
+    //Control de checkbox de Spa
+    const [spaService, setSpaService] = useState(false);
+    const handleOnChangeSpaCheck = () => setSpaService(!spaService);
+  
     return (
-        <div className={styles.popupFondo}>
-            <div className={styles.popupDiv}>
-                <h3>Resumen de la reserva</h3>
-                <div className={styles.popupFechasDiv}>
-                    <label>
-                        Fecha de entrada:
-                        <input className="popupFechas" type="date" value={fecha1.toISOString().split("T")[0]} disabled />
-                    </label>
-                    <label>
-                        Fecha de salida:
-                        <input className="popupFechas" type="date" value={fecha2.toISOString().split("T")[0]} disabled />
-                    </label>
-                </div>
-                <h5>Ocupantes: {ocupantes}</h5>
-                <p><strong>Adultos: </strong>{adultos}</p>
-                <p><strong>Niños (menores de 12 años): </strong>{ninyos}</p>
-                <hr />
-                <h5>
-                  <label>
-                      Número de habitaciones:
-                      <input type="number" name="roomNumber" min="1" max="3" defaultValue="1" />
-                  </label>
-                </h5>
-                <h5>Tipo de habitación:</h5> {/* los tipos de habitaciones para elegir */}
-                <label>
-                    <input type="radio" name="roomType" value="basic" />
-                    Habitación Básica:
-                    <p>
-                      Descripción de la habitación e imágenes
-                      <br />
-                      Precio
-                    </p>
-                </label>
-                <label>
-                    <input type="radio" name="roomType" value="premium" />
-                    Habitación Premium:
-                    <p>
-                      Descripción de la habitación e imágenes
-                      <br />
-                      Precio
-                    </p>
-                </label>
-                <label>
-                    <input type="radio" name="roomType" value="suite" />
-                    Suite:
-                    <p>
-                      Descripción de la habitación e imágenes
-                      <br />
-                      Precio
-                    </p>
-                </label>
-            </div>
+      <div className={styles.popupFondo}>
+        <div className={styles.popupDiv}>
+          <h3>Resumen de la reserva</h3>
+          <div className={styles.popupFechasDiv}>
+            <label>
+              Fecha de entrada:
+              <input
+                className={styles.popupFechas}
+                type='date'
+                value={fecha1.toISOString().split('T')[0]}
+                disabled
+              />
+            </label>
+            <label>
+              Fecha de salida:
+              <input
+                className={styles.popupFechas}
+                type='date'
+                value={fecha2.toISOString().split('T')[0]}
+                disabled
+              />
+            </label>
+            <label>
+              Noches:
+              <input
+                className={styles.popupFechas}
+                type='number'
+                name='roomNumber'
+                value={noches}
+                disabled
+              />
+            </label>
+          </div>
+          <h5>Huéspedes: {ocupantes}</h5>
+          <p>
+            <strong>Adultos: </strong>{adultos}
+            <strong> Niños (menores de 12 años): </strong>
+            {ninyos}
+          </p>
+          <hr />
+          <h3>Habitaciones y Servicios</h3>
+          <h5>
+            <label>
+              Número de habitaciones:
+              <input
+                type='number'
+                name='roomNumber'
+                min='1'
+                max='3'
+                defaultValue='1'
+              />
+            </label>
+          </h5>
+          <h5>Tipo de habitación:</h5>
+          {/* los tipos de habitaciones para elegir, con unas descripción e imágenes además del precio y la disponibilidad */}
+          <label>
+            <input type='radio' name='roomType' value='standard' />
+            <strong> Habitación Estándar: </strong>
+            <p>
+              Descripción de la habitación e imágenes
+              <br />
+              Precio: {'300€'} Disponibles: {3}
+            </p>
+          </label>
+          <label>
+            <input type='radio' name='roomType' value='premium' />
+            <strong> Habitación Premium: </strong>
+            <p>
+              Descripción de la habitación e imágenes
+              <br />
+              Precio: {'300€'} Disponibles: {3}
+            </p>
+          </label>
+          <label>
+            <input type='radio' name='roomType' value='suite' />
+            <strong> Suite: </strong>
+            <p>
+              Descripción de la habitación e imágenes
+              <br />
+              Precio: {'300€'} Disponibles: {3}
+            </p>
+          </label>
+          <label>
+              <input type="checkbox" checked={spaService} onChange={handleOnChangeSpaCheck} />
+              Spa & Wellness Center
+          </label>
+          <div className={styles.popupBoton}>
+            <button
+              className={styles.popupBotonReservar}
+              onClick={() => alert('Reserva realizada con éxito!')}
+            >
+              Confirmar reserva
+            </button>
+          </div>
         </div>
-    )
+      </div>
+        )
 }
