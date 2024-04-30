b69c732795a01eeb68e57db90efa0e201830cd40
Pablo Fernández <pablofergon92@gmail.com>
2024-04-30 10:15:44 +0200

    reol

--- develhope-group-app/src/main.jsx
+++ develhope-group-app/src/main.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
 
->>>>>>> TermsConditions-&-SPA
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
