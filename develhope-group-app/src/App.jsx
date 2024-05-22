587ccb8cd402c5934189b68c0947d86eba55a08f
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-05-22 12:46:13 +0100

    App.jsx Prettier fix

--- develhope-group-app/src/App.jsx
+++ develhope-group-app/src/App.jsx
@@ -1,5 +1,5 @@
 import "../src/Styles/App.css";
-import { Routes, Route ,useLocation} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import TermsConditions from "./Pages/TermsConditions";
 import Navbar from "./Components/Navbar";
 import AboutUs from "./Pages/AboutUs";
@@ -12,67 +12,66 @@ import NotFound from "./Components/NotFound";
 import SpaPage from "./Pages/Spa";
 import Banner from "./Components/Banner";
 import BookingMenu from "./Components/BookingMenu";
-// import { GlobalProvider } from './GlobalContext';
+import { GlobalProvider } from "./GlobalContext";
 import Rooms from "./Pages/Rooms";
 import NavbarMobile from "./Components/NavbarMobile";
 import { useEffect, useState } from "react";
-import Booking from "./Pages/Booking"
-import CookiesBanner from "./Components/Cookies.jsx";//NUEVO*****
-import Login from "./Pages/Login";//NUEVO*****
-import BotonLogin from "./Components/BotonLogin";//NUEVO*****
-import {UserGlobalProvider} from "/src/globalvariable/Usuarioglobal.jsx"
-import Gallery from "./Pages/Gallery.jsx";//NUEVO*****
+import Booking from "./Pages/Booking";
+import CookiesBanner from "./Components/Cookies.jsx"; //NUEVO*****
+import Login from "./Pages/Login"; //NUEVO*****
+import BotonLogin from "./Components/BotonLogin"; //NUEVO*****
+import { UserGlobalProvider } from "/src/globalvariable/Usuarioglobal.jsx";
+import Gallery from "./Pages/Gallery.jsx"; //NUEVO*****
 
 export default function App() {
-// NO MOSTRAR BOTON_LOGIN EN PAGINA LOGIN
+  // NO MOSTRAR BOTON_LOGIN EN PAGINA LOGIN
   const location = useLocation();
-  const showLogin = !location.pathname.includes('/Login');
-  const showBanner = !location.pathname.includes('/habitaciones');
+  const showLogin = !location.pathname.includes("/Login");
+  const showBanner = !location.pathname.includes("/habitaciones");
 
   // NO MOSTRAR BOTON_LOGIN EN PAGINA LOGIN
 
-  
-    const [showNavbarMobile, setShowNavbarMobile] = useState(false)
+  const [showNavbarMobile, setShowNavbarMobile] = useState(false);
 
   useEffect(() => {
     const handleMobile = () => {
       setShowNavbarMobile(window.innerWidth <= 680); //no pararle al número, es mi cinta scotch :D
     };
 
-    handleMobile(); 
+    handleMobile();
 
-    window.addEventListener('resize', handleMobile);
+    window.addEventListener("resize", handleMobile);
 
     return () => {
-        window.removeEventListener('resize', handleMobile);
+      window.removeEventListener("resize", handleMobile);
     };
-}, []);
+  }, []);
 
   return (
-    <UserGlobalProvider> 
-    <>
-      {showNavbarMobile && <NavbarMobile/>} 
-      <CookiesBanner/>
-      {/* {showBotonLogin && <BotonLogin/>}  */}
-      {showLogin &&   <Navbar /> }
-      {showLogin &&    <BookingMenu /> }
-      {showBanner &&       <Banner /> }
-      <ScrollUp />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Habitaciones" element={<Rooms />} />
-        <Route path="/nosotros" element={<AboutUs />} />
-        <Route path="/Terms&Conditions" element={<TermsConditions />} />
-        <Route path="/restauranteSunset" element={<Dining />} />
-        <Route path="/test" element={<ContactPrompt />} />
-        <Route path="/Booking" element={<Booking/>} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/spa" element={<SpaPage />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Galeria" element={<Gallery />} />
-      </Routes>
-      {showLogin && <Footer />}
-    </>
-    </UserGlobalProvider> 
+    <UserGlobalProvider>
+      <>
+        {showNavbarMobile && <NavbarMobile />}
+        <CookiesBanner />
+        {/* {showBotonLogin && <BotonLogin/>}  */}
+        {showLogin && <Navbar />}
+        {showLogin && <BookingMenu />}
+        {showBanner && <Banner />}
+        <ScrollUp />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Habitaciones" element={<Rooms />} />
+          <Route path="/nosotros" element={<AboutUs />} />
+          <Route path="/Terms&Conditions" element={<TermsConditions />} />
+          <Route path="/restauranteSunset" element={<Dining />} />
+          <Route path="/test" element={<ContactPrompt />} />
+          <Route path="/Booking" element={<Booking />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/spa" element={<SpaPage />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Galeria" element={<Gallery />} />
+        </Routes>
+        {showLogin && <Footer />}
+      </>
+    </UserGlobalProvider>
   );
 }
